0998e45efed7e382514fb19f245af3c7e2ae2f09
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-12-14 01:36:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div >
+        <button
+          className="btn btn-secondary"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </button>
+        
+      </div>
+     
+    </>
+  );
+}
+
+export default App;
